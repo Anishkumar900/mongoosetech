b6b712a3284d8d695e5427cc8859b1172f1c3cb2
Anish <hit19ece.anishkumar@gmail.com>
2024-05-08 09:41:14 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import UseContextHook from './UseContextHook';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UseContextHook/>
+     <BrowserRouter basename='/mongoosetech'>
+     <Routes>
+      <Route path='/' element={<UseContextHook/>}/>
+     </Routes>
+     </BrowserRouter>
+      
+    
   </React.StrictMode>
 );
 
